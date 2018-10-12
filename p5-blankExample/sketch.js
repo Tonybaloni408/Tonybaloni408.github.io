f3603f553688e102bb3cc0acfc55022240d6e834
Tonybaloni408 <43894536+Tonybaloni408@users.noreply.github.com>
2018-10-12 14:38:46 -0700

    Update p5-blankExample/sketch.js

--- p5-blankExample/sketch.js
+++ p5-blankExample/sketch.js
@@ -4,11 +4,12 @@
 function setup() {
   // put setup code here --> this runs once upon launch
 
+
   createCanvas(500, 500);
   background(0);
 }
-
-
+// draw ellipse
+ellipse(50, 50, 80, 80);
 function draw() {
   // put drawing code here --> this loops every frame
 }
